115e346d091b61ea8a10e30c09a73fe7a563dc59
ArthurV261 <93939824+ArthurV261@users.noreply.github.com>
2022-05-13 14:48:40 -0300

    Create AreaTriangulo.cs

---
+++ Aula 02/AreaTriangulo.cs
@@ -0,0 +1,17 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Digite o valor da base");
+      string s = Console.ReadLine();
+    double b = double.Parse(s);
+    Console.WriteLine("Digite o valor da altura");
+    string r = Console.ReadLine();
+    double h = double.Parse(r);
+
+      double a = b * h / 2;
+
+    Console.WriteLine($"Área do triâgulo = {a:0.00}");
+  }
+}
