44522dd2209a99b4800f2b2b4c7739c1228f56cd
Arthur Victor da Silva Nascimento <silva.arthur@academico.ifrn.edu.br>
2022-10-24 15:51:19 -0300

    Upload de Abastecimento

---
+++ AppCombustivel/AppCombustivel/Abastecimento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCombustivel
+{
+    class Abastecimento
+    {
+        private double KmRodado;
+        private double ValorLitro;
+        private double ValorPago;
+
+        public void SetKmRodado(double KmRodado)
+        {
+            if (KmRodado > 0) this.KmRodado = KmRodado;
+        }
+        public void SetValorPago(double ValorPago)
+        {
+            if (ValorPago > 0) this.ValorPago = ValorPago;
+        }
+        public void SetValorLitro(double ValorLitro)
+        {
+            if (ValorLitro > 0) this.ValorLitro = ValorLitro;
+        }
+        public double MediaKmLitro()
+        {
+            double numLitros = ValorPago / ValorLitro;
+            return KmRodado / numLitros;
+        }
+        public double MediaReaisKm()
+        {
+            return ValorPago / KmRodado;
+        }
+    }
+}
---
+++ AppCombustivel/AppCombustivel/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppCombustivel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecimento obj = new Abastecimento();
+            obj.SetKmRodado(double.Parse(txtKmRodado.Text));
+            obj.SetValorLitro(double.Parse(txtValorLitro.Text));
+            obj.SetValorPago(double.Parse(txtValorPago.Text));
+            txtKmLitro.Text = obj.MediaKmLitro().ToString("0.00");
+            txtRListro.Text = obj.MediaReaisKm().ToString("0.00");
+        }
+    }
+}
